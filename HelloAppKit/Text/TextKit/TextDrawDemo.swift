0852cdc40756569ef3a97acc37aff2a564a93cdd
drypot <drypot@gmail.com>
2025-07-23 18:09:39 +0900

    update TextDrawDemo

--- HelloAppKit/Text/TextKit/TextDrawDemo.swift
+++ HelloAppKit/Text/TextKit/TextDrawDemo.swift
@@ -17,97 +17,128 @@ class TextDrawDemo: NSViewController {
         let view = NSView()
         self.view = view
 
-        let stringDrawDemo = StringDrawDemo()
-        stringDrawDemo.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stringDrawDemo)
-
-        let layoutManagerDemo = LayoutManagerDemo()
-        layoutManagerDemo.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(layoutManagerDemo)
+        let stackView = NSStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.orientation = .vertical
+        stackView.alignment = .leading
+        view.addSubview(stackView)
 
         NSLayoutConstraint.activate([
             view.widthAnchor.constraint(greaterThanOrEqualToConstant: 400),
             view.heightAnchor.constraint(greaterThanOrEqualToConstant: 400),
 
-            stringDrawDemo.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            stringDrawDemo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stringDrawDemo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            stringDrawDemo.heightAnchor.constraint(equalToConstant: 80),
-
-            layoutManagerDemo.topAnchor.constraint(equalTo: stringDrawDemo.topAnchor, constant: 80),
-            layoutManagerDemo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            layoutManagerDemo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            layoutManagerDemo.heightAnchor.constraint(equalToConstant: 80),
-
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
         ])
+
+        let stringDrawDemo = StringDrawDemo()
+        stackView.addArrangedSubview(stringDrawDemo)
+
+        let layoutManagerDemo = LayoutManagerDemo()
+        stackView.addArrangedSubview(layoutManagerDemo)
     }
 
     class StringDrawDemo: NSView {
-        override func draw(_ dirtyRect: NSRect) {
-            super.draw(dirtyRect)
-
-            let text = "NSString Draw" as NSString
-            let attributes: [NSAttributedString.Key: Any] = [
-                .font: NSFont.preferredFont(forTextStyle: .title3),
-                .foregroundColor: NSColor.black
-            ]
-
-            let textSize = text.size(withAttributes: attributes)
-            let textOrigin = NSPoint(
-                x: (bounds.width - textSize.width) / 2,
-                y: (bounds.height - textSize.height) / 2
-            )
-
-            text.draw(at: textOrigin, withAttributes: attributes)
-        }
-    }
-
-    class LayoutManagerDemo: NSView {
-
-        private var textStorage = NSTextStorage()
-        private var layoutManager = NSLayoutManager()
-        private var textContainer: NSTextContainer
+        let text = "StringDrawDemo"
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: NSFont.preferredFont(forTextStyle: .title1),
+            .foregroundColor: NSColor.black
+        ]
 
         override init(frame frameRect: NSRect) {
-            let attributedString = NSAttributedString(
-                string: "NSLayoutManager drawGlyphs",
-                attributes: [
-                    .font: NSFont.systemFont(ofSize: 24),
-                    .foregroundColor: NSColor.black
-                ]
-            )
-            textStorage.setAttributedString(attributedString)
-
-            textContainer = NSTextContainer(size: frameRect.size)
-            textContainer.lineFragmentPadding = 5.0
-
-            layoutManager.addTextContainer(textContainer)
-            textStorage.addLayoutManager(layoutManager)
-
             super.init(frame: frameRect)
+            wantsLayer = true
+            layer?.backgroundColor = NSColor.textBackgroundColor.cgColor
         }
 
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
 
+        override var intrinsicContentSize: NSSize {
+            return NSSize(width: 400, height: 120)
+        }
+
         override func draw(_ dirtyRect: NSRect) {
             super.draw(dirtyRect)
 
-            let textRange = layoutManager.glyphRange(for: textContainer)
-            let usedRect = layoutManager.usedRect(for: textContainer)
-            let textOrigin = NSPoint(
-                x: (bounds.width - usedRect.width) / 2,
-                y: (bounds.height - usedRect.height) / 2
-            )
+            print("\(type(of: self)) bounds: \(bounds)")
 
-            layoutManager.drawGlyphs(forGlyphRange: textRange, at: textOrigin)
+            let textSize = text.size(withAttributes: attributes)
+            let textOrigin = NSPoint(
+                x: 0,
+                y: bounds.height - textSize.height
+            )
+//            let textOrigin = NSPoint(
+//                x: (bounds.width - textSize.width) / 2,
+//                y: (bounds.height - textSize.height) / 2
+//            )
+
+            text.draw(at: textOrigin, withAttributes: attributes)
+        }
+    }
+
+    class LayoutManagerDemo: NSView {
+        let text = "LayoutManagerDemo"
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: NSFont.preferredFont(forTextStyle: .title1),
+            .foregroundColor: NSColor.black
+        ]
+
+        private var textStorage = NSTextStorage()
+        private var layoutManager = NSLayoutManager()
+        private var textContainer = NSTextContainer(size: .zero)
+
+        override init(frame frameRect: NSRect) {
+            let attrString = NSAttributedString( string: text, attributes: attributes )
+
+            textStorage.setAttributedString(attrString)
+
+            textContainer.lineFragmentPadding = 5.0
+
+            textStorage.addLayoutManager(layoutManager)
+            layoutManager.addTextContainer(textContainer)
+
+            super.init(frame: frameRect)
+
+            wantsLayer = true
+            layer?.backgroundColor = NSColor.textBackgroundColor.cgColor
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override var intrinsicContentSize: NSSize {
+            return NSSize(width: 400, height: 120)
         }
 
         override func layout() {
             super.layout()
             textContainer.size = bounds.size
-            //            setNeedsDisplay(bounds)
+        }
+
+        override func draw(_ dirtyRect: NSRect) {
+            super.draw(dirtyRect)
+
+            print("\(type(of: self)) bounds: \(bounds)")
+
+            let glyphRange = layoutManager.glyphRange(for: textContainer)
+            let usedRect = layoutManager.usedRect(for: textContainer)
+            let textOrigin = NSPoint(
+                x: 0,
+                y: bounds.height - usedRect.height
+            )
+//            let textSize = text.size(withAttributes: attributes)
+//            let textOrigin = NSPoint(
+//                x: 0,
+//                y: bounds.height - textSize.height
+//            )
+
+            // drawGlyphs at 의 기분 위치는 line fragment origin 이다.
+            layoutManager.drawGlyphs(forGlyphRange: glyphRange, at: textOrigin)
         }
     }
 
