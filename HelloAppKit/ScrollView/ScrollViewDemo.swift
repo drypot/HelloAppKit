44047f14445d8a8da045bb6fc4caeb7d45465dcc
drypot <drypot@gmail.com>
2025-08-25 01:20:19 +0900

    update ScrollViewDemo

--- HelloAppKit/ScrollView/ScrollViewDemo.swift
+++ HelloAppKit/ScrollView/ScrollViewDemo.swift
@@ -19,65 +19,62 @@ class ScrollViewDemo: NSViewController {
         let view = NSView()
         self.view = view
 
-        // setup ScrollView
+        let documentView = CustomView()
 
         let scrollView = NSScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.hasVerticalScroller = true
-        scrollView.drawsBackground = false;
+        scrollView.documentView = documentView
+        scrollView.drawsBackground = false
 
         view.addSubview(scrollView)
 
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
         ])
-
-        // setup documentView
-
-        let documentView = NSView()
-        documentView.translatesAutoresizingMaskIntoConstraints = false
-
-        scrollView.documentView = documentView
-
-        var constrainsts = [NSLayoutConstraint]()
-
-        var lastItem: NSView?
-        for i in 0..<128 {
-            let label = NSTextField(labelWithString: "\(i)")
-            label.translatesAutoresizingMaskIntoConstraints = false
-            documentView.addSubview(label)
-
-            constrainsts.append(contentsOf: [
-                label.leadingAnchor.constraint(equalTo: documentView.leadingAnchor, constant: 20)
-            ])
-            if let lastItem {
-                constrainsts.append(
-                    label.topAnchor.constraint(equalTo: lastItem.bottomAnchor, constant: 8)
-                )
-            } else {
-                constrainsts.append(
-                    label.topAnchor.constraint(equalTo: documentView.topAnchor, constant: 20)
-                )
-            }
-
-            lastItem = label
-        }
-
-        constrainsts.append(contentsOf: [
-            documentView.topAnchor.constraint(equalTo: scrollView.contentView.topAnchor),
-            documentView.leadingAnchor.constraint(equalTo: scrollView.contentView.leadingAnchor),
-            documentView.trailingAnchor.constraint(equalTo: scrollView.contentView.trailingAnchor),
-
-            // documentView.bottomAnchor 를 scrollView 에 걸지 말고
-            // documentView 의 크기를 확정할 수 있는 constraint 들을 추가한다.
-
-            documentView.bottomAnchor.constraint(equalTo: lastItem!.bottomAnchor, constant: 20),
-        ])
-
-        NSLayoutConstraint.activate(constrainsts)
     }
 
+
+}
+
+fileprivate class CustomView: NSView {
+
+    private let count = 128
+    private let fieldWidth: CGFloat = 200
+    private let rowHeight: CGFloat = 24
+    private let sidePadding: CGFloat = 12
+    private let topBottomPadding: CGFloat = 12
+
+    override init(frame frameRect: NSRect) {
+        let width = sidePadding * 2 + fieldWidth
+        let height = topBottomPadding * 2 + CGFloat(count) * rowHeight
+        super.init(frame: NSRect(x: 0, y: 0, width: width, height: height))
+
+        self.wantsLayer = true
+        self.layer?.backgroundColor = NSColor.quaternarySystemFill.cgColor
+
+        buildChildren()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override var isFlipped: Bool { true }
+
+    func buildChildren() {
+        for i in 0..<count {
+            let y = topBottomPadding + CGFloat(i) * rowHeight
+            let frame = NSRect(x: sidePadding, y: y, width: fieldWidth, height: rowHeight)
+
+            let tf = NSTextField(labelWithString: "\(i)")
+            tf.alignment = .left
+            tf.frame = frame
+
+            addSubview(tf)
+        }
+    }
 }
